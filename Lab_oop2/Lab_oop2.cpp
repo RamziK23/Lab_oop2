d48eb58e5788ec96e4ce680b7e3e8ed9a61b93e9
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-10-22 12:40:17 +0500

    V1.2

--- Lab_oop2/Lab_oop2.cpp
+++ Lab_oop2/Lab_oop2.cpp
@@ -26,11 +26,37 @@ public:
 	}
 };
 
+class Line {
+public:
+	int x1, y1, x2, y2;
+	Line(int x1, int y1, int x2, int y2) {
+		Point p1(x1, y1);
+		Point p2(x2, y2);
+		printf("Line(int x1, int y1, int x2, int y2)\n");
+	}
+	Line(const Line& l) {
+		printf("Line(const Line &p)\n");
+		x1 = l.x1;
+		y1 = l.y1;
+		x2 = l.x2;
+		y2 = l.y2;
+	}
+	~Line() {
+		/*printf("%d,%d,%d,%d\n", x1, y1, x2, y2);*/
+		printf("~Line\n");
+	}
+
+};
+
 int main(){
 {
 	Point p;
 	Point p2(10, 20);
 	Point p3(p2);
+}
+{
+	Line l1(5, 10, 15, 20);
+	Line l2(l1);
 }
 _getch();
 return 0;
