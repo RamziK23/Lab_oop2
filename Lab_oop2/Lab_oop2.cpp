04b2179c53fd0b1ca382ec72364b8d7129d84406
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-10-23 11:59:24 +0500

    V1.4

--- Lab_oop2/Lab_oop2.cpp
+++ Lab_oop2/Lab_oop2.cpp
@@ -1,70 +1,92 @@
 ﻿#include <iostream>
 #include <conio.h>
-
 #include <iostream>
-class Point {
+using namespace std;
+
+class Point {           //класс точка
 protected:
 	int x, y;
 public:
 	
 	Point() {
-		printf("Point()\n");
+		printf("Пустая точка\n");                  //конструктор путой точки
 		x = 0;
 		y = 0;
 	}
 	Point(int x, int y) {
-		printf("Point(int x, int y)\n");
+		printf("Точка с координатами\n");          //конструктор точки с координатами
 		this->x = x;
 		this->y = y;
 	}
 	Point(const Point& p) {
-		printf("Point(const Point &p)\n");
+		printf("Точка-копия\n");                   //копирующий конструктор
 		x = p.x;
 		y = p.y;
 	}
 	~Point() {
-		printf("%d, %d\n", x, y);
-		printf("~Point()\n");
+		printf("Удаление точки с координатами %d, %d\n", x, y);                  //диструктор
 	}
-	void move(int dx,int dy) {
+	void move(int dx,int dy) {                     //метод move
 		x = x + dx;
 		y = y + dy;
 	}
+	void reset();
 };
 
-//class Line {
-//public:
-//	int x1, y1, x2, y2;
-//	Line(int x1, int y1, int x2, int y2) {
-//		Point p1(x1, y1);
-//		Point p2(x2, y2);
-//		printf("Line(int x1, int y1, int x2, int y2)\n");
-//	}
-//	Line(const Line& l) {
-//		printf("Line(const Line &p)\n");
-//		x1 = l.x1;
-//		y1 = l.y1;
-//		x2 = l.x2;
-//		y2 = l.y2;
-//	}
-//	~Line() {
-//		/*printf("%d,%d,%d,%d\n", x1, y1, x2, y2);*/
-//		printf("~Line\n");
-//	}
-//
-//};
+void Point::reset() {
+	x = 0;
+	y = 0;
+}
+
+class Line: public Point {
+protected:
+	Point *p1;
+	Point *p2;
+	int x2, y2;
+public:
+	Line() {
+		p1 = new Point;
+		p2 = new Point;
+		printf("Пустая линия\n");
+	}
+	Line(int x, int y, int x2, int y2) {
+		Point p1(x, y);
+		Point p2(x2, y2);
+		printf("Линия\n");
+	}
+	Line(const Line& l) {
+		printf("Линия-копия\n");
+		x = l.x;
+		y = l.y;
+		x2 = l.x2;
+		y2 = l.y2;
+	}
+	~Line() {
+		delete p1;
+		delete p2;
+		printf("Удаление линии с координатами %d,%d,%d,%d\n", x, y, x2, y2);
+
+	}
+
+};
 
 int main(){
-	Point* p = new Point(5, 10);
-	Point p2(1, 2);
-	Point p3(p2);
-	p->move(10, 10);
-	delete p;
-
+	setlocale(LC_ALL, "Russian");
 //{
-//	Line l1(5, 10, 15, 20);
-//	Line l2(l1);
+//	Point p0;
+//	Point* p = new Point(5, 10);
+//	Point p2(1, 2);
+//	Point p3(p2);
+//	p->reset();
+//	p->move(10, 10);
+//	delete p;
 //}
+
+{
+	Line l0;
+	Line l1(5, 10, 15, 20);
+	Line l2(l1);
+}
 //_getch();
 return 0;
 };
